30fbd8441185e14d6dd77d45646082de3785e9d0
Hans Naert <hans.naert@vives.be>
2024-09-16 19:17:31 +0200

    Change logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo-vives.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.vives.be"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vives
         </a>
       </header>
     </div>
