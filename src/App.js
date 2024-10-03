9ee505e586ebe8a28c5d5adac58e4d52174e9176
Hans Naert <hans.naert@vives.be>
2024-10-03 22:11:07 +0200

    JSX loop via map

--- src/App.js
+++ src/App.js
@@ -15,19 +15,15 @@ function App() {
           <button>Click</button>
         </p>
         {
-          (function () {
-            var jsx = [];
-            for (var i = 0; i < 3; i++) {
-              jsx.push(
-                <React.Fragment key={i}>
-                  Counter {i} defined by {`<Counter init='5' end='${10 + i}' />`} : <br />
-                  <Counter init='5' end={10 + i} />
-                  <br /><br />
-                </React.Fragment>
-              );
-            }
-            return jsx;            
-          })()
+          [0, 1, 2].map(function (i) {
+            return (
+              <React.Fragment key={i}>
+                Counter {i} defined by {`<Counter init='5' end='${10 + i}' />`} : <br />
+                <Counter init='5' end={10 + i} />
+                <br /><br />
+              </React.Fragment>
+            )
+          })
         }
         <a
           className="App-link"
