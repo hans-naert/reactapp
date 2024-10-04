e086c31c8f8119419da20f72776a2e5c9514f259
Hans Naert <hans.naert@vives.be>
2024-10-04 22:13:11 +0200

    useReducer hook do not modify the original state in the reducer

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import React, { useState, createContext } from 'react';
 const TotalContext = createContext();
 
 function App() {
-  const [total, setTotal] = useState(0);
+  
   return (
     <>
-    <TotalContext.Provider value={[total, setTotal]}>
       <Counter/> <hr />
       <Counter focus={true}/> <hr />
       <Counter/> <hr />
-      <b>Total : {total}</b>
-    </TotalContext.Provider>
     </>
   );
 }
 
 export default App;
-export { TotalContext };
--- src/Counter.js
+++ src/Counter.js
@@ -1,36 +1,25 @@
-import { useState, useRef, useEffect, useContext } from "react";
-import { TotalContext } from "./App";
-function Counter({ focus }) {
-    const [value, setValue] = useState("");
-    const [total, setTotal] = useContext(TotalContext);
-    const refCounter = useRef();
-    useEffect(function () {
-        if (focus) refCounter.current.focus(); // refCounter.current represents the DOM element associated with that reference
-    },[]);
-
-    function change(event) {
-        var newValue = parseInt(event.target.value || 0);
-        // New value in the field
-        setValue(newValue);
-        // New Total
-        setTotal((total) => (total - value)); // Subtract the old value
-        setTotal((total) => (total + newValue)); // Add the new value
+import { useReducer } from "react";
+function Counter() {
+    const [state, dispatch] = useReducer(function (state, action) {
+        var newState={...state}; //do not modify the original state
+        if (action == "INCR") newState.value += 1;
+        if (action == "DECR") newState.value -= 1;
+        return newState;
+    }, { value: 0 });
+    function incrValue() {
+        dispatch("INCR");
     }
-
-    function keydown(event) {
-        // Display the pressed key in the console
-        console.log(event.key);
-        // Allow the Backspace, Delete, ArrowLeft, ArrowRight, and Tab keys
-        if (["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Tab"].includes(event.key)) return;
-        // Then disallow all other keys except those from 0 to 9
-        if (event.key < "0" || event.key > "9") event.preventDefault();
+    function decrValue() {
+        dispatch("DECR");
     }
-
     return (
         <>
-            Counter : <input type="text" onChange={change} onKeyDown={keydown} ref={refCounter} /> <br />
-            <br />
-            Input Value : {value}
+            <button onClick={incrValue}>value + 1</button>
+            &nbsp;&nbsp;
+            <button onClick={decrValue}>value - 1</button>
+            &nbsp;
+            =&gt;
+            value = {state.value};
         </>
     )
 }
